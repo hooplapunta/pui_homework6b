028ec451e519dbfd9230ac3e9d39bc8b7fec3c0b
Rong Kang Chew <chewrongkang@gmail.com>
2021-04-06 22:38:07 -0400

    switched to using defined objects

---
+++ js/cart.js
@@ -0,0 +1,7 @@
+// function for loading from storage
+
+// function for saving from storage
+
+// function for clearing cart
+
+// attach to onload, loading, number display
--- js/productitems.js
+++ js/productitems.js
@@ -1,5 +1,40 @@
-var toAddToCart = 0;
+// product item definition
+function Option(name, description) {
+    this.name = name;
+    this.description = description;
+}
 
+function Item(name, description, size, cost) {
+    this.name = name;
+    this.description = description;
+    this.size = size;
+    this.cost = cost;
+}
+
+
+// donut definitions
+var donutOptions = [
+    new Option("almond", "Head Baker Tom’s special creation was made when he toasted almonds for a little too long, \
+but this donut has a wonderful smokiness, especially when paired with our signature toffee glaze."),
+    new Option("classic", "Despite this donut having just the simple classic sugary glaze, our humble beginnings \
+    uses the finest vanilla and sugar that is also sustainably sourced!"),
+    new Option("chocolate","Nothing is quite like this decadant treat of a luscious glaze atop any donut, \
+    made with a mix of both 70% dark and milk chocolate, plus a hint of spice."),
+    new Option("pink", '"Hey, hey you", Pink Donut says. "Are you feeling craaaaazy?!" \
+    Our crazy pink donut does not actually talk, but we believe that is what it would say. '),
+    new Option("strawberry", "An innocent looking sugar-dusted donut this looks, but once you bite into \
+    the center, expect an explosion of delicious homemade strawberry jelly."),
+    new Option("mango",  "Is it mango? Is it lime? What we do know that this donut is a mystery. \
+    We are pretty sure it tastes different every bite, but it will always be moreish.")
+]
+
+var donutItems = [
+    new Item("Box of 1 Dozen", null, 12, 19.99),
+    new Item("Box of 1/2 Dozen", null, 6, 10.99),
+    new Item("Donuts in bag", null, 1, 1.99),
+]
+
+// hide all plus and minus buttons until hovered over
 $(".menuItem").find("button").hide();
 
 $(".menuItem").on("mouseover", function (event) {
@@ -8,32 +43,7 @@ $(".menuItem").on("mouseover", function (event) {
     var id = $(this).attr("id")
     var text = "All our donuts are baked fresh daily. Select any donut to learn more about all of Donut Theory's doughy personality."
 
-    switch (id) {
-        case "almond":
-            text = "Head Baker Tom’s special creation was made when he toasted almonds for a little too long, \
-            but this donut has a wonderful smokiness, especially when paired with our signature toffee glaze."
-            break;
-        case "classic":
-            text = "Despite this donut having just the simple classic sugary glaze, our humble beginnings \
-            uses the finest vanilla and sugar that is also sustainably sourced!"
-            break;
-        case "chocolate":
-            text = "Nothing is quite like this decadant treat of a luscious glaze atop any donut, \
-            made with a mix of both 70% dark and milk chocolate, plus a hint of spice."
-            break;
-        case "pink":
-            text = '"Hey, hey you", Pink Donut says. "Are you feeling craaaaazy?!" \
-            Our crazy pink donut does not actually talk, but we believe that is what it would say. '
-            break;
-        case "strawberry":
-            text = "An innocent looking sugar-dusted donut this looks, but once you bite into \
-            the center, expect an explosion of delicious homemade strawberry jelly."
-            break;
-        case "mango":
-            text = "Is it mango? Is it lime? What we do know that this donut is a mystery. \
-            We are pretty sure it tastes different every bite, but it will always be moreish."
-            break;
-    }
+    text = _.find(donutOptions, {'name': id}).description;
 
     $("#description").html(text)
 });
@@ -42,6 +52,7 @@ $(".menuItem").on("mouseleave", function (event) {
     $(this).find("button").hide()
 });
 
+// update totals when amounts change
 $(".menuItem").find("input").on("change", updateAmount);
 
 $(".up-button").on("click", function (event) {
@@ -60,40 +71,43 @@ $(".down-button").on("click", function (event) {
     updateAmount();
 });
 
-function updateAmount (event) {
+// update amount in add to cart button
+var toAddToCart = 0;
+
+function updateAmount(event) {
     var totalDonuts = $(".menuItem").find("input").toArray().reduce((a, b) => a + parseInt(b.value), 0)
     $("#donutCount").html(totalDonuts)
     console.log("totalDonuts", totalDonuts)
 
-    var sizes = [
-        {
-            size: 12,
-            unit: "Box of 1 Dozen",
-            cost: 19.99
-        },
-        {
-            size: 6,
-            unit: "Box of 1/2 Dozen",
-            cost: 10.99
-        },
-        {
-            size: 1,
-            unit: "donuts in bag",
-            cost: 1.99
-        }
-    ]
+    // var sizes = [
+    //     {
+    //         size: 12,
+    //         unit: "Box of 1 Dozen",
+    //         cost: 19.99
+    //     },
+    //     {
+    //         size: 6,
+    //         unit: "Box of 1/2 Dozen",
+    //         cost: 10.99
+    //     },
+    //     {
+    //         size: 1,
+    //         unit: "donuts in bag",
+    //         cost: 1.99
+    //     }
+    // ]
 
 
     var totalSizes = [];
     var totalCost = 0;
-    
+
     while (totalDonuts > 0) {
-        for (size of sizes) {
-            if (size.size <= totalDonuts) {
-                console.log("remove donuts", size.size);
-                totalDonuts -= size.size;
-                totalSizes.push(size);
-                totalCost += size.cost;
+        for (item of donutItems) {
+            if (item.size <= totalDonuts) {
+                console.log("remove donuts", item.size);
+                totalDonuts -= item.size;
+                totalSizes.push(item);
+                totalCost += item.cost;
                 break;
             }
         }
@@ -102,9 +116,9 @@ function updateAmount (event) {
     toAddToCart = totalSizes.length;
 
     var finalUnits = "";
-    var totalUnits =  _.countBy(totalSizes, "unit");
+    var totalUnits = _.countBy(totalSizes, "name");
     for (key in totalUnits) {
-        finalUnits +=  totalUnits[key] +" " + key +"<br>"
+        finalUnits += totalUnits[key] + " " + key + "<br>"
     }
     $("#donutUnits").html(finalUnits)
 }
@@ -112,3 +126,4 @@ function updateAmount (event) {
 $(".addToCartButton").on("click", function (event) {
     $("#cartCount").html(parseInt($("#cartCount").html()) + toAddToCart);
 });
+
